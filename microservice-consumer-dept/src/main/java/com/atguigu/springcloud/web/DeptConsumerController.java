5f44c1954a492a55444b019479d8c9aac0faea6b
Draghu <13923154647@163.com>
2018-11-07 17:43:44 +0800

    update

--- microservice-consumer-dept/src/main/java/com/atguigu/springcloud/web/DeptConsumerController.java
+++ microservice-consumer-dept/src/main/java/com/atguigu/springcloud/web/DeptConsumerController.java
@@ -2,10 +2,7 @@ package com.atguigu.springcloud.web;
 
 import com.atguigu.springcloud.entity.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -18,7 +15,7 @@ import java.util.List;
 @RestController
 public class DeptConsumerController {
 
-    private static final String REST_URL_PREFIX = "http://localhost:8001";
+    private static final String REST_URL_PREFIX = "http://MICROSERVICE-PROVIDER-DEPT";
 
     @Autowired
     private RestTemplate restTemplate;
--- microservice-provider-dept-8002/src/main/java/com/atguigu/springcloud/DeptProvider2App.java
+++ microservice-provider-dept-8002/src/main/java/com/atguigu/springcloud/DeptProvider2App.java
@@ -2,16 +2,18 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Draghu
- * @date 2018/11/7 10:03
+ * @date 2018/11/6 18:23
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class DeptConsumerApp {
+@EnableDiscoveryClient
+public class DeptProvider2App {
     public static void main(String[] args) {
-        SpringApplication.run(DeptConsumerApp.class, args);
+        SpringApplication.run(DeptProvider2App.class, args);
     }
 }
